b6e429a436e27d76e9ac41da64f0ce9629e4b102
Dren Jaha <78754907+DrenJaha@users.noreply.github.com>
2022-11-03 16:09:25 -0400

    Update BookStoreSearch.java

--- BookStoreSearch.java
+++ BookStoreSearch.java
@@ -2,7 +2,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
-class TestCase1 {
+class BookStoreSearch {
 	
 	//test whether the search function returns a book 
 	@Test
