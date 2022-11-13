4a86adda784698c50fc160daeb58bd52ddbf0648
Elias <60025074+EliasTkla@users.noreply.github.com>
2022-11-13 05:46:06 -0500

    Update UpdateBookAvailability.java
    Added a new test case at test3 to test input of 0

--- UpdateBookAvailability.java
+++ UpdateBookAvailability.java
@@ -35,17 +35,28 @@ class UpdateBookAvailability {
 		Bookstore bs1 = new Bookstore("UWindsor", 100);
 		
 		bs1.add(b1);
-		assertEquals("The Book 'Intro to agile 1' has no stock to purchase from", bs1.updateAvailability("Intro to agile 1", -5));
+		assertEquals("No updates made", bs1.updateavailability("Intro to agile 1", 0));
 	}
 	
 	@Test
-	//testing if inputted book name is not within inventory.
+	//updating Book stock quantity below 0
 	void test4() {
 		Book b1 = new Book("Intro to agile 1", 0);
 		
 		Bookstore bs1 = new Bookstore("UWindsor", 100);
 		
 		bs1.add(b1);
-		assertEquals("Book does not exist within the inventory", bs1.updateAvailability("Intro to algorithms", -5));
+		assertEquals("The Book 'Intro to agile 1' has no stock to purchase from", bs1.updateavailability("Intro to agile 1", -5));
+	}
+	
+	@Test
+	//testing if wrong book name is given
+	void test5() {
+		Book b1 = new Book("Intro to agile 1", 0);
+		
+		Bookstore bs1 = new Bookstore("UWindsor", 100);
+		
+		bs1.add(b1);
+		assertEquals("Book does not exist within the inventory", bs1.updateavailability("Intro to algorithms", -5));
 	}
 }
