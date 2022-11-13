0c40ec7f33e745d20ff537716d6c4fc182838bc8
Elias <60025074+EliasTkla@users.noreply.github.com>
2022-11-13 15:49:40 -0500

    Update UpdateBookAvailability.java
    Made some changes to test more input cases

--- UpdateBookAvailability.java
+++ UpdateBookAvailability.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 class UpdateBookAvailability {
 	 
 	@Test
-		//updating Book stock to available 
+	//testing the input of 0 in quantity to add 
 	void test1() {
 		Book b1 = new Book("Intro to agile 1", 0);
 		
@@ -13,45 +13,57 @@ class UpdateBookAvailability {
 		
 		bs1.add(b1);
 		
-		assertEquals("The Book 'Intro to agile 1' is now in stock with 5 Books available for purchase", bs1.updateAvailability("Intro to agile 1", 5));
+		assertEquals("No updates made", bs1.updateavailability("Intro to agile 1", 0));
+	}
+	
+	@Test
+		//testing the increase on book stock
+	void test2() {
+		Book b1 = new Book("Intro to agile 1", 0);
+		
+		Bookstore bs1 = new Bookstore("UWindsor", 100);
+		
+		bs1.add(b1);
+		
+		assertEquals("The Book 'Intro to agile 1' now has 5 books in stock", bs1.updateavailability("Intro to agile 1", 5));
 	}
 
 	@Test
-		//updating Book stock to not available
-	void test2() {
+		//testing the withdrawal of more books than available
+	void test3() {
 		Book b1 = new Book("Intro to agile 1", 5);
 		
 		Bookstore bs1 = new Bookstore("UWindsor", 100);
 		
 		bs1.add(b1);
-		assertEquals("The Book 'Intro to agile 1' is now out of stock", bs1.updateAvailability("Intro to agile 1", -5));
+		assertEquals("The Book 'Intro to agile 1' has insufficent stock", bs1.updateavailability("Intro to agile 1", -10));
 	}
 	
 	@Test
-	//updating Book stock quantity below 0
-	void test3() {
-		Book b1 = new Book("Intro to agile 1", 0);
-		
-		Bookstore bs1 = new Bookstore("UWindsor", 100);
-		
-		bs1.add(b1);
-		assertEquals("No updates made", bs1.updateavailability("Intro to agile 1", 0));
-	}
-	
-	@Test
-	//updating Book stock quantity below 0
+	//testing the withdrawal of all books in stock
 	void test4() {
+		Book b1 = new Book("Intro to agile 1", 5);
+		
+		Bookstore bs1 = new Bookstore("UWindsor", 100);
+		
+		bs1.add(b1);
+		assertEquals("The Book 'Intro to agile 1' is now out of stock", bs1.updateavailability("Intro to agile 1", -5));
+	}
+	
+	@Test
+	//testing withdrawal of book when stock is at 0
+	void test5() {
 		Book b1 = new Book("Intro to agile 1", 0);
 		
 		Bookstore bs1 = new Bookstore("UWindsor", 100);
 		
 		bs1.add(b1);
-		assertEquals("The Book 'Intro to agile 1' has no stock to purchase from", bs1.updateavailability("Intro to agile 1", -5));
+		assertEquals("The Book 'Intro to agile 1' is not in stock", bs1.updateavailability("Intro to agile 1", -5));
 	}
 	
 	@Test
-	//testing if wrong book name is given
-	void test5() {
+	//testing when book name is not within inventory
+	void test6() {
 		Book b1 = new Book("Intro to agile 1", 0);
 		
 		Bookstore bs1 = new Bookstore("UWindsor", 100);
